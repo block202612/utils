4aa619d8a2a0e90d0009fa19676c8eee1709945d
block202612 <crunchcpt897@gmail.com>
2018-01-31 07:20:25 +0000

    [i] - Simple script to change license in src code

---
+++ changelicense.py
@@ -0,0 +1,31 @@
+import os
+import sys
+
+path=sys.argv[1]
+license_to_replace="// Copyright (c) 2014-2017, The Monero Project"
+license=           "// Copyright (c) 2014-2017, The Monero Project"
+
+counter=0
+
+path_files_container=[]
+for root, directories, filenames in os.walk(path):
+        for filename in filenames:
+                  path_files_container.append(os.path.join(root,filename))
+
+for item in path_files_container:
+        rstream=open(item,"r")
+        source=rstream.read()
+        if license_to_replace in source:
+               counter+=1
+               source=source.replace(license_to_replace,license)
+               tmp_filename=item+".xxxxx"
+               wstream=open(tmp_filename,"w")
+               wstream.write(source)
+               os.remove(item)
+               filename=tmp_filename.replace(".xxxxx","")
+               os.rename(tmp_filename,filename)
+
+               
+
+                  
+            
