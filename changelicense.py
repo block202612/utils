509383c86a51bc7fed0eeea81e75ec73a8213215
block202612 <crunchcpt897@gmail.com>
2018-02-01 19:55:20 +0000

    Initial stable release

--- changelicense.py
+++ changelicense.py
@@ -2,30 +2,43 @@ import os
 import sys
 
 path=sys.argv[1]
-license_to_replace="Copyright (c) 2017, edollar project (fork from Monero)"
-license=           "Copyright (c) 2014-2017, The Monero Project"
+license_to_replace="Copyright (c) 2017, edollar project"# (fork from Monero)"
+license_line1=           "// Copyright (c) 2014-2017, The Monero Project\n"
+license_line2=           "// Copyright (c) 2017-2018, The Monero Project\n"
 
 counter=0
 
-path_files_container=[]
+files_container=[]
 for root, directories, filenames in os.walk(path):
         for filename in filenames:
-                  path_files_container.append(os.path.join(root,filename))
+                  files_container.append(os.path.join(root,filename))
 
-for item in path_files_container:
-        rstream=open(item,"r")
-        source=rstream.read()
-        if license_to_replace in source:
-               counter+=1
-               source=source.replace(license_to_replace,license)
-               tmp_filename=item+".xxxxx"
-               wstream=open(tmp_filename,"w")
-               wstream.write(source)
-               os.remove(item)
-               filename=tmp_filename.replace(".xxxxx","")
-               os.rename(tmp_filename,filename)
-
-               
-
+for filename in files_container:
+        rstream=open(filename,"r")
+        lines=rstream.readlines()
+        tmp_source_lines=[]
+        flag=False 
+        for line in lines:
+             if license_to_replace in line:
+                  flag=True
+                  counter+=1
+                  print line
                   
-            
+                  tmp_source_lines.append(license_line1)
+                  tmp_source_lines.append(license_line2)
+                  
+                #  line=line.replace(license_to_replace,license)
+                 # tmp_filename=filename+".xxxxx"
+                  #wstream=open(tmp_filename,"w")
+                  #wstream.write(source)
+                  #os.remove(item)
+                  #filename=tmp_filename.replace(".xxxxx","")
+                  #os.rename(tmp_filename,filename)
+             else:
+                  tmp_source_lines.append(line)
+        if flag==True:
+             tmp_filename=filename+".xxxxx"
+             wstream=open(tmp_filename,"w")
+             for line in tmp_source_lines:
+                  newline=line#[:-1]
+                  wstream.write("%s" % newline)
